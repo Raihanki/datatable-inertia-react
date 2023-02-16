2afe2c44d52d65a2bc0f44a7a900cbfbf5519ab7
Raihan Khoerul Imam <raihanhori101@gmail.com>
2023-02-16 20:37:43 +0700

    wip

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,9 +8,20 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index()
+    public $limit = 15;
+    public function index(Request $request)
     {
-        $users = UserResource::collection(User::paginate());
+        $query = User::query();
+        if ($request->has('search')) {
+            $query->where('username', 'like', '%' . $request->search . '%');
+        }
+        $users = (UserResource::collection($query->paginate($request->limit))
+        )->additional([
+            'options' => [
+                'limit' => $request->limit ?? $this->limit,
+                'page' => $request->page ?? 1,
+            ]
+        ]);
         return inertia('User/Index', [
             "users" => $users
         ]);
--- resources/js/Pages/User/Index.jsx
+++ resources/js/Pages/User/Index.jsx
@@ -1,8 +1,31 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Inertia } from "@inertiajs/inertia";
 import { Head, Link } from "@inertiajs/inertia-react";
+import { debounce, pickBy } from "lodash";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Index(props) {
-    const { data: peoples, meta } = props.users;
+    const { data: peoples, meta, options } = props.users;
+    const [params, setParams] = useState(options);
+
+    const onChange = (e) =>
+        setParams({ ...params, [e.target.name]: e.target.value });
+
+    const reload = useCallback(
+        debounce((query) => {
+            Inertia.get(
+                route("users.index"),
+                { ...pickBy(query), page: query.search ? 1 : query.page },
+                {
+                    preserveState: true,
+                }
+            );
+        }, 150),
+        []
+    );
+
+    useEffect(() => reload(params), [params]);
+
     return (
         <AuthenticatedLayout
             auth={props.auth}
@@ -18,6 +41,33 @@ export default function Index(props) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="flex w-full justify-end px-3 gap-2">
+                            <div className="mt-2 flex-col gap-3 w-1/6">
+                                <label htmlFor="limit">Limit</label>
+                                <select
+                                    name="limit"
+                                    id="limit"
+                                    value={params.limit}
+                                    onChange={onChange}
+                                    className="rounded-md px-3 py-2 w-full form-select"
+                                >
+                                    <option value="10">10</option>
+                                    <option value="25">25</option>
+                                    <option value="50">50</option>
+                                </select>
+                            </div>
+                            <div className="mt-2 flex-col gap-3 w-1/4">
+                                <label htmlFor="limit">Search</label>
+                                <input
+                                    type={"text"}
+                                    name="search"
+                                    id="search"
+                                    value={params.search}
+                                    onChange={onChange}
+                                    className="rounded-md px-3 py-2 w-full form-input"
+                                ></input>
+                            </div>
+                        </div>
                         <div className="p-2">
                             <div className="overflow-x-auto p-3">
                                 <table className="table-auto w-full">
@@ -52,7 +102,7 @@ export default function Index(props) {
                                     </thead>
 
                                     <tbody className="text-sm divide-y divide-gray-100">
-                                        {peoples.length &&
+                                        {peoples.length > 0 ? (
                                             peoples.map((user, index) => (
                                                 <tr key={meta.from + index}>
                                                     <td className="p-3">
@@ -81,18 +131,43 @@ export default function Index(props) {
                                                         </div>
                                                     </td>
                                                 </tr>
-                                            ))}
+                                            ))
+                                        ) : (
+                                            <div>Data Not Found</div>
+                                        )}
                                     </tbody>
                                 </table>
                                 {/* pagination */}
                                 <div className="flex items-center mt-10">
                                     {meta.links.map((link, index) => (
-                                        <Link
-                                            key={index}
+                                        // <Link
+                                        //     key={index}
+                                        //     disabled={
+                                        //         link.url == null ? true : false
+                                        //     }
+                                        //     as="button"
+                                        //     className={`${
+                                        //         link.url == null
+                                        //             ? "text-gray-500 bg-gray-100"
+                                        //             : "text-gray-900  bg-gray-50"
+                                        //     } rounded-md flex items-center justify-center border border-gray-200 px-4 py-2`}
+                                        //     href={link.url || ""}
+                                        // >
+                                        //     {link.label}
+                                        // </Link>
+                                        <button
                                             disabled={
                                                 link.url == null ? true : false
                                             }
-                                            as="button"
+                                            onClick={() =>
+                                                setParams({
+                                                    ...params,
+                                                    page: new URL(
+                                                        link.url
+                                                    ).searchParams.get("page"),
+                                                })
+                                            }
+                                            key={index}
                                             className={`${
                                                 link.url == null
                                                     ? "text-gray-500 bg-gray-100"
@@ -101,7 +176,7 @@ export default function Index(props) {
                                             href={link.url || ""}
                                         >
                                             {link.label}
-                                        </Link>
+                                        </button>
                                     ))}
                                 </div>
                             </div>
